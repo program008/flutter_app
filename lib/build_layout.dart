e09e70bd5008eae9a016311b39a9da468174e7dc
liutao <tao.liu@uurbot.cn>
2019-11-06 11:52:36 +0800

    update build_layout

--- lib/build_layout.dart
+++ lib/build_layout.dart
@@ -14,6 +14,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class MyWidget extends StatelessWidget {
   Widget textSection = Container(
     padding: const EdgeInsets.all(32),
@@ -78,11 +123,12 @@ class MyWidget extends StatelessWidget {
           ),
         ),
         /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+//        Icon(
+//          Icons.star,
+//          color: Colors.red[500],
+//        ),
+//        Text('41'),
+        FavoriteWidget()
       ],
     ),
   );
